76c70ae2155d98b65f7941e271be180ce9d57fae
whai2 <whai2125@gmail.com>
2024-02-26 19:16:31 +0900

    [노은수] feat : toast ui 구현

---
+++ src/components/toast/Toast.jsx
@@ -0,0 +1,11 @@
+import './Toast.css'
+
+const Toast = () => {
+  return (
+    <div className="toast">
+      <span className="toastText">URL이 복사되었습니다</span>
+    </div>
+  )
+}
+
+export default Toast
