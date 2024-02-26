6f59dd590baf1fb059cc69c56c896bfabcff1fe4
김동현 <dtc03003@naver.com>
2024-02-26 16:55:22 +0900

    [김동현] feat : 테스트 라우터 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import KDH from "./components/kdh/KDH";
+import NES from "./components/nes/NES";
+import KYE from "./components/kye/KYE";
+import KYH from "./components/kyh/KYH";
+import PGB from "./components/pgb/PGB";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/kdh" element={<KDH />}></Route>
+        <Route path="/nes" element={<NES />}></Route>
+        <Route path="/kye" element={<KYE />}></Route>
+        <Route path="/kyh" element={<KYH />}></Route>
+        <Route path="/pgb" element={<PGB />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
