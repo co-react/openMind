24d4a3a989fe1ad7a55b40c7118513d23a0a27fc
beom <gb3187@naver.com>
2024-02-27 13:15:30 +0900

    [박기범] feat : badge 버튼 구현

--- src/components/badge/AnswerBtn.jsx
+++ src/components/badge/AnswerBtn.jsx
@@ -1,9 +1,9 @@
-import "./PGB.css";
+import "./AnswerBtn.css";
 
 //일단 다른 컴포넌트에서 해당 게시글에 대한 답변이 되었다면
 //isAnswered를 true로 아니면 false로 전달하면 될것 같습니다.
 
-function PGB({ isAnswered = true }) {
+function AnswerBtn({ isAnswered }) {
   return isAnswered ? (
     <button className="badgeAns">답변완료</button>
   ) : (
@@ -11,4 +11,4 @@ function PGB({ isAnswered = true }) {
   );
 }
 
-export default PGB;
+export default AnswerBtn;
