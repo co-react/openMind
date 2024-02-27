a3fdd569136d6d5eb3557d41acaaa3ffae3c7ce9
영은\Hi <popduddms@gmail.com>
2024-02-27 17:22:42 +0900

    [김영은] rename : 폴더 경로 변경

--- src/components/dropdown/Dropdown.jsx
+++ src/components/dropdown/Dropdown.jsx
@@ -1,40 +1,40 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import arrowDownIcon from '../../assets/svg/Icons/arrowDown.svg';
-import arrowUpIcon from '../../assets/svg/Icons/arrowUp.svg';
+import arrowDownIcon from "../../assets/svg/icons/arrow-down.svg";
+import arrowUpIcon from "../../assets/svg/icons/arrow-up.svg";
 
-import './Dropdown.css'
+import "./Dropdown.css";
 
 function Dropdown() {
   const [isOpened, setIsOpened] = useState(false);
 
   const handleClick = () => {
     setIsOpened(!isOpened);
-  }
+  };
 
   if (isOpened) {
     return (
-      <div className='dropdown'>
-        <span className='dropdownMenuTitle'>이름순</span>
-        <img src={arrowUpIcon} onClick={handleClick} alt='접기'/>
-        <div className='dropdownMenus'>
-          <div className='dropdownMenu'>
-            <span className='dropdownMenuTitle'>이름순</span>
+      <div className="dropdown">
+        <span className="dropdownMenuTitle">이름순</span>
+        <img src={arrowUpIcon} onClick={handleClick} alt="접기" />
+        <div className="dropdownMenus">
+          <div className="dropdownMenu">
+            <span className="dropdownMenuTitle">이름순</span>
           </div>
           <div>
-            <span className='dropdownMenuTitle'>최신순</span> 
+            <span className="dropdownMenuTitle">최신순</span>
           </div>
         </div>
       </div>
-    ) // 추후에 메뉴는 props, map의 조합으로 작성할 예정입니다.
+    ); // 추후에 메뉴는 props, map의 조합으로 작성할 예정입니다.
   }
 
   return (
-  <div className='dropdown'>
-    <span className='dropdownMenuTitle'>이름순</span>
-    <img src={arrowDownIcon} onClick={handleClick} alt='펼치기'/>
-  </div>
-  ) 
+    <div className="dropdown">
+      <span className="dropdownMenuTitle">이름순</span>
+      <img src={arrowDownIcon} onClick={handleClick} alt="펼치기" />
+    </div>
+  );
 }
 
-export default Dropdown
+export default Dropdown;
--- src/components/reactions/Hate.jsx
+++ src/components/reactions/Hate.jsx
@@ -1,27 +1,29 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import hateIcon from '../../assets/icons/thumbs-down.svg';
+import hateIcon from "../../assets/svg/icons/thumbs-down.svg";
 
-import './Like.css';
+import "./Like.css";
 
 function Hate() {
   const [isReacted, setIsReacted] = useState(false);
 
   const handleClick = () => {
     setIsReacted(!isReacted);
-  }
+  };
 
   const clickStyle = {
-    color : isReacted ? '#000' : '#818181',
-    filter: isReacted || 'invert(56%) sepia(1%) saturate(1741%) hue-rotate(3deg) brightness(89%) contrast(86%)'
-  }
+    color: isReacted ? "#000" : "#818181",
+    filter: isReacted || "invert(56%) sepia(1%) saturate(1741%) hue-rotate(3deg) brightness(89%) contrast(86%)",
+  };
 
   return (
-    <div className="like" type='button' onClick={handleClick}>
-      <img src={hateIcon} alt='싫어요 아이콘' style={clickStyle}/>
-      <span className='likeText' style={clickStyle}>싫어요</span>
+    <div className="like" type="button" onClick={handleClick}>
+      <img src={hateIcon} alt="싫어요 아이콘" style={clickStyle} />
+      <span className="likeText" style={clickStyle}>
+        싫어요
+      </span>
     </div>
-  )
+  );
 }
 
-export default Hate
+export default Hate;
