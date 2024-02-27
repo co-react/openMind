5613246d42af39e02f628d4c65e6bf812fe908cc
영은\Hi <popduddms@gmail.com>
2024-02-27 17:20:28 +0900

    [김영은] feat : input 컴포넌트 생성

---
+++ src/components/kye/KYE.jsx
@@ -0,0 +1,16 @@
+import Form from "../input/Form";
+import Input from "../input/Input";
+import InputField from "../input/InputField";
+import InputTextArea from "../input/InputTextArea";
+
+function KYE() {
+  return (
+    <Form>
+      <Input />
+      <InputField />
+      <InputTextArea />
+    </Form>
+  );
+}
+
+export default KYE;
