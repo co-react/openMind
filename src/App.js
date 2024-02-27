25ab81721fae9e4f1e2432e47ad438ce2d066602
김동현 <dtc03003@naver.com>
2024-02-27 17:59:15 +0900

    [김동현] rename : 영현님 파일 삭제

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import KDH from "./components/kdh/KDH";
 import NES from "./components/nes/NES";
 import KYE from "./components/kye/KYE";
-import KYH from "./components/kyh/KYH";
 import PGB from "./components/pgb/PGB";
 import MainTest from "./pages/MainTest";
 
@@ -23,7 +22,6 @@ function App() {
         <Route path="/kdh" element={<KDH />}></Route>
         <Route path="/nes" element={<NES />}></Route>
         <Route path="/kye" element={<KYE />}></Route>
-        <Route path="/kyh" element={<KYH />}></Route>
         <Route path="/pgb" element={<PGB />}></Route>
       </Routes>
     </BrowserRouter>
