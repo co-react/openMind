69dec65b11d3461d779d70a40a2270c5e75e46ed
beom <gb3187@naver.com>
2024-02-27 16:06:12 +0900

    [박기범] refactor : 불필요한 import문 삭제 변수명 클래스명 수정

--- src/components/usercard/UserCard.jsx
+++ src/components/usercard/UserCard.jsx
@@ -1,16 +1,15 @@
-import React from "react";
 import messageIcon from "../../assets/svg/Icons/messageIcon.svg";
 import "./UserCard.css";
 
 //프로필 사진과 프로필의 이름,
 //그리고 9개라고 적힌것은 props로 받아야 할 것 같습니다.
 
-function UserCard({ proFileImg, proFileName, questionCount }) {
+function UserCard({ profileImg, profileName, questionCount }) {
   return (
     <div className="userCard">
-      <div className="proFile">
-        <img src={proFileImg} alt="프로필 사진" />
-        <p>화면 출력용{proFileName}</p>
+      <div className="profile">
+        <img src={profileImg} alt="프로필 사진" />
+        <p>화면 출력용{profileName}</p>
       </div>
       <div className="question">
         <div className="questionReceived">
