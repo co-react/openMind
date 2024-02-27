a2e6ed26bc055ab9c8e733fc674b2ed83b4ba401
영은\Hi <popduddms@gmail.com>
2024-02-28 00:26:49 +0900

    [김영은] feat : Input 컴포넌트 완료

--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -4,7 +4,6 @@ const Input = styled.input`
   padding: 1.2rem 1.6rem;
   font-size: 1.6rem;
   outline: none;
-
   border-radius: 0.8rem;
   border: 0.1rem solid var(--Grayscale-40, #818181);
   background: var(--Grayscale-10, #fff);
