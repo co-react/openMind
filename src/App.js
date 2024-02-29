df2032adcf562ebfc17606422f749e31176f4c01
영은\Hi <popduddms@gmail.com>
2024-02-29 13:37:16 +0900

    [김영은] fix : GlobalStyle설정 완료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import "./style/style.css";
 import GlobalStyles from "./style/GlobalStyles";
 
 import KDH from "./components/kdh/KDH";
--- src/components/input/Form.jsx
+++ src/components/input/Form.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { BoxShadow1 } from "../../style/commonStyles";
 
 const Form = styled.form`
   ${BoxShadow1}
