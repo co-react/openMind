7ed80531b314a3d69e8c9b35c807920db1cdcb2a
김동현 <dtc03003@naver.com>
2024-02-29 16:24:53 +0900

    [김동현] fix : 좋아요, 싫어요 폰트 12-> 14px

--- src/components/reactions/Hate.jsx
+++ src/components/reactions/Hate.jsx
@@ -15,9 +15,7 @@ function Hate() {
   return (
     <Button isClicked={isReacted} onClick={handleClick}>
       <ThumbsDown />
-      <ButtonText>
-        싫어요
-      </ButtonText>
+      <ButtonText>싫어요</ButtonText>
     </Button>
   );
 }
@@ -26,17 +24,19 @@ export default Hate;
 
 const ButtonText = styled.span`
   color: var(--Grayscale-40, #818181);
-  font-feature-settings: 'clig' off, 'liga' off;
+  font-feature-settings:
+    "clig" off,
+    "liga" off;
   font-family: Pretendard;
-  font-size: 1.2rem;
+  font-size: 1.4rem;
   font-style: normal;
   font-weight: 500;
   line-height: 1.2rem;
-`
+`;
 
 const ThumbsDown = styled(hateIcon)`
   filter: invert(54%) sepia(0%) saturate(9%) hue-rotate(156deg) brightness(93%) contrast(82%);
-`
+`;
 
 const Button = styled.button`
   display: flex;
@@ -44,13 +44,16 @@ const Button = styled.button`
   gap: 6px;
   border: none;
   background-color: #ffffff;
-  ${props => props.isClicked && css`
-    ${ButtonText} {
-      color: #000000;
-    }
+  ${(props) =>
+    props.isClicked &&
+    css`
+      ${ButtonText} {
+        color: #000000;
+      }
 
-    ${ThumbsDown} {
-      filter: invert(0%) sepia(100%) saturate(7500%) hue-rotate(61deg) brightness(106%) contrast(109%);
-    }
-  `}
-`
+      ${ThumbsDown} {
+        filter: invert(0%) sepia(100%) saturate(7500%) hue-rotate(61deg) brightness(106%)
+          contrast(109%);
+      }
+    `}
+`;
