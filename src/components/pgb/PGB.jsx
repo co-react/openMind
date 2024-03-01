34d262842050a8b3cd32f5d94450d96a7c667918
beom <gb3187@naver.com>
2024-03-01 18:55:08 +0900

    [박기범] feat : 카드리스트 불러오기 api 테스팅

--- src/components/pgb/PGB.jsx
+++ src/components/pgb/PGB.jsx
@@ -1,36 +1,25 @@
-import { styled } from "styled-components";
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "../../apis/axios";
+import requests from "../../apis/request";
 
-//일단 다른 컴포넌트에서 해당 게시글에 대한 답변이 되었다면
-//isAnswered를 true로 아니면 false로 전달하면 될것 같습니다.
+function PGB() {
+  const [cardList, setCardList] = useState({});
 
-function AnswerButton({ isAnswered = true }) {
-  return (
-    <StyledButton isAnswered={isAnswered}>
-      {isAnswered ? "답변 완료" : "미답변"}
-    </StyledButton>
-  );
+  useEffect(() => {
+    async function getCardList() {
+      try {
+        const response = await axios.get(requests.SUBJECTS);
+        //console.log(response);
+        setCardList(response);
+        console.log(cardList);
+      } catch (error) {
+        console.error("에러 발생:", error);
+      }
+    }
+
+    getCardList();
+  }, []);
 }
 
-export default AnswerButton;
-
-const StyledButton = styled.button`
-  display: inline-flex;
-  padding: 4px 12px;
-  justify-content: center;
-  align-items: center;
-  gap: 10px;
-  border-radius: 8px;
-  background: var(--Grayscale-10, #fff);
-  font-feature-settings: "clig" off, "liga" off;
-  font-family: Pretendard;
-  font-size: 14px;
-  font-style: normal;
-  font-weight: 500;
-  line-height: 18px; /* 128.571% */
-  border: ${({ isAnswered }) =>
-    isAnswered
-      ? "1px solid var(--Brown-40, #542f1a)"
-      : "1px solid var(--Grayscale-40, #818181)"};
-  color: ${({ isAnswered }) =>
-    isAnswered ? "var(--Brown-40, #542f1a)" : "var(--Grayscale-40, #818181)"};
-`;
+export default PGB;
