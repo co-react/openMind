0ec81bfdeefb242e119ae76a36ee0c28f358340d
whai2 <whai2125@gmail.com>
2024-03-01 16:13:28 +0900

    [노은수] feat : 로고 컴포넌트 제거, 로고 파일 직접 호출, 스타일 지정

--- src/pages/CardPage.jsx
+++ src/pages/CardPage.jsx
@@ -1,7 +1,28 @@
+import styled from 'styled-components';
+
+import { ReactComponent as LogoIcon } from '../assets/svg/logo.svg';
+
 function CardPage() {
   return (
-    <div>CardPage</div>
+    <Layout>
+      <SmallStyledLogo />
+    </Layout>
   )
 }
 
 export default CardPage
+
+const Layout = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const SmallStyledLogo = styled(LogoIcon)`
+  display: flex;
+  width: 124px;
+  height: 49px;
+  justify-content: center;
+  align-items: center;
+  flex-shrink: 0;
+`
