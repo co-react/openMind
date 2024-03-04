13a7d33de1df8b4d97dd7e0be603e4a694a5d741
beom <gb3187@naver.com>
2024-03-04 18:58:33 +0900

    [박기범] feat : PGB의 작성 내용을 ListPage에 적용

--- src/pages/ListPage.jsx
+++ src/pages/ListPage.jsx
@@ -1,6 +1,12 @@
+import { useState } from "react";
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { styled } from "styled-components";
+import axios from "../apis/axios";
+import requests from "../apis/request";
 import logo from "../assets/svg/icons/logo.svg";
+import { ReactComponent as ArrowLeftIcon } from "../assets/svg/icons/arrow-left.svg";
+import { ReactComponent as ArrowRightIcon } from "../assets/svg/icons/arrow-right.svg";
 //import { ReactComponent as NextArrow } from "../assets/svg/icons/next.svg";
 import BaseButton from "../components/buttons/Button";
 import Dropdown from "../components/dropdown/Dropdown";
@@ -9,6 +15,102 @@ import Pagination from "../components/pagination/Pagination";
 import UserCard from "../components/userCard/UserCard";
 
 function ListPage() {
+  const [cardList, setCardList] = useState([]);
+  //const [nextUrl, setNextUrl] = useState("");
+  const [cards, setCards] = useState(0); //총 카드 수 28개
+  const [pages, setPages] = useState(0); //총 페이지 수
+  const [startPage, setStartPage] = useState(1); //시작 페이지
+  const [endPage, setEndPage] = useState(2); //끝 페이지 (수정 필요)
+  const [clickedPage, setClickedPage] = useState(1); //누른 페이지 숫자
+  const [offset, setOffset] = useState(0);
+  const [limit, setLimit] = useState(8);
+  const [sortUrl, setSortUrl] = useState("&sort=time");
+
+  //오프셋 리미트를 위한 코드
+  const offsetUrl = `?limit=${limit}&offset=${offset}`;
+
+  useEffect(() => {
+    async function getCardList() {
+      try {
+        const response = await axios.get(
+          requests.SUBJECTS + offsetUrl + sortUrl
+        );
+        setCardList(response.data);
+        //setNextUrl(response.data.next);
+        setCards(Number(response.data.count));
+        setPages(Math.ceil(cards / 8)); // 총 페이지 수 계산
+        setLimit(8);
+        //setOffset(8);
+        console.log(1);
+      } catch (error) {
+        console.error("에러 발생:", error);
+      }
+    }
+
+    getCardList();
+  }, [offset, sortUrl]);
+
+  //페이지네이션 랜더링 함수
+  const renderPageNumbers = () => {
+    const pageNumbers = [];
+    for (let i = startPage; i <= endPage; i++) {
+      pageNumbers.push(
+        <Pagination
+          isSelected={i === clickedPage}
+          onClick={() => handleClickPage(i)}
+          key={i}
+        >
+          {i}
+        </Pagination>
+      );
+    }
+    return pageNumbers;
+  };
+
+  //이름순 최신순을 고르면 URL이 바뀌는 코드
+  //option값을 조건을 주어 sortUrl변경
+  const handleSortOption = (option) => {
+    if (option == "이름순") {
+      setSortUrl("&sort=name");
+    } else {
+      setSortUrl("&sort=time");
+    }
+  };
+
+  //페이지 네이션의 각 페이지 숫자를 클릭하면 실행되는 함수
+  const handleClickPage = (i) => {
+    setOffset(8 * i - 8);
+    setClickedPage(i);
+  };
+
+  //페이지 네이션의 왼쪽 화살표를 누르면 실행되는 함수
+  const handleBeforePage = () => {
+    if (startPage == 1) {
+      return;
+    }
+    setStartPage(startPage - 2);
+    setEndPage(endPage - 2);
+    setClickedPage(startPage - 2);
+    setOffset(8 * (startPage - 2) - 8);
+  };
+
+  //페이지 네이션의 오른쪽 화살표를 누르면 실행되는 함수
+  const handleAfterPage = () => {
+    if (endPage == pages) {
+      return;
+    }
+    setStartPage(startPage + 2);
+    setClickedPage(startPage + 2);
+    setOffset(8 * (startPage + 2) - 8);
+    //이 케이스는 페이지네이션을 일정한 값을 더해가면서 넘기는데
+    //기존의 총 페이지 수보다 클 경우를 대비
+    if (endPage + 2 > pages) {
+      setEndPage(pages);
+    } else {
+      setEndPage(endPage + 2);
+    }
+  };
+
   return (
     <Container>
       <HeaderDiv>
@@ -23,22 +125,27 @@ function ListPage() {
       </HeaderDiv>
       <TitleDiv>
         <Title>누구에게 질문할까요?</Title>
-        <Dropdown />
+        <Dropdown onSelect={handleSortOption} />
       </TitleDiv>
       <CardListDiv>
-        <UserCard />
-        <UserCard />
-        <UserCard />
-        <UserCard />
-        <UserCard />
-        <UserCard />
-        <UserCard />
+        {cardList.results &&
+          cardList.results.map((item) => (
+            <>
+              <UserCard
+                key={item.id}
+                profileImg={item.imageSource}
+                profileName={item.name}
+                questionCount={item.questionCount}
+              ></UserCard>
+              {/* <p>{cards}</p>
+              <p>{nextUrl}</p> */}
+            </>
+          ))}
       </CardListDiv>
       <PaginationDiv>
-        <Pagination />
-        <Pagination />
-        <Pagination />
-        <Pagination />
+        <StyledArrowLeftIcon onClick={handleBeforePage} />
+        {renderPageNumbers()}
+        <StyledArrowRightIcon onClick={handleAfterPage} />
       </PaginationDiv>
     </Container>
   );
@@ -109,4 +216,26 @@ const Title = styled.p`
   font-style: normal;
   font-weight: 400;
   line-height: normal;
+`;
+
+const StyledArrowLeftIcon = styled(ArrowLeftIcon)`
+  color: var(--Grayscale-40, #818181);
+  text-align: center;
+  font-feature-settings: "clig" off, "liga" off;
+  font-family: Actor;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 25px; /* 125% */
+`;
+
+const StyledArrowRightIcon = styled(ArrowRightIcon)`
+  color: var(--Grayscale-40, #818181);
+  text-align: center;
+  font-feature-settings: "clig" off, "liga" off;
+  font-family: Actor;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 25px; /* 125% */
 `;
