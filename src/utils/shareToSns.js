8f2a9ca4a5d3fe0060fdbba957ca85b1d79ec175
whai2 <whai2125@gmail.com>
2024-03-05 18:45:39 +0900

    [노은수] feat : 페이스북 공유 기능 구현

--- src/utils/shareToSns.js
+++ src/utils/shareToSns.js
@@ -1,5 +1,8 @@
-export function shareToFacebook() {
+const FACEBOOK_SHARE_URL = "https://www.facebook.com/sharer/sharer.php?href=";
+const SMALL_WINDOW_SIZE = 'width=400 ,height=800';
 
+export function shareToFacebook() {
+  window.open(FACEBOOK_SHARE_URL,'공유하기' ,SMALL_WINDOW_SIZE)
 }
 
 export function shareToKakao() {
