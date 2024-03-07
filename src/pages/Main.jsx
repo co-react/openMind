31e1e5222ccb00ce1ced873497c6c870b4709ce3
영은\Hi <popduddms@gmail.com>
2024-03-08 00:52:13 +0900

    [김영은] feat : 메인이미지 삽입

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-import mainImg from "../assets/png/main.png";
+import mainImg from "../assets/png/guys.png";
 import { ReactComponent as Logo } from "../assets/svg/icons/logo.svg";
 import Button from "../components/buttons/ArrowIconButton";
 import Stack from "../components/common/Stack";
@@ -16,7 +16,9 @@ function Main() {
         질문하러 가기
       </GoToASK>
       <CreateQuestionCard />
-      <MainImg src={mainImg} />
+      <ImgContainer>
+        <MainImg src={mainImg} />
+      </ImgContainer>
     </Container>
   );
 }
@@ -48,6 +50,27 @@ const GoToASK = styled(Button)`
   }
   @media (min-width: 1200px) {
     right: 13rem;
+  }
+`;
+
+const ImgContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  width: 100%;
+  position: absolute;
+  top: 25rem;
+  height: 30rem;
+  overflow: hidden;
+  z-index: -1;
+
+  @media (min-width: 768px) {
+    top: 38rem;
+    height: 50rem;
+  }
+  @media (min-width: 1200px) {
+    top: 7.5rem;
+    height: 120rem;
   }
 `;
 
