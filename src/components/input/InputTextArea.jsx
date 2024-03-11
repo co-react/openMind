ed40ecc796f28c37e00d1953852740570e6bd209
김동현 <dtc03003@naver.com>
2024-03-11 15:04:07 +0900

    [김동현] feat : InputTextArea 다크모드 색상 추가

--- src/components/input/InputTextArea.jsx
+++ src/components/input/InputTextArea.jsx
@@ -8,9 +8,13 @@ const InputTextArea = styled.textarea`
   height: 32.6rem;
   border-radius: 0.8rem;
   border: 0.1rem solid transparent;
-  background: var(--Grayscale-20, #f9f9f9);
+  background: ${(props) => props.theme.colors.colorInput};
+  color: ${(props) => props.theme.colors.colorMainFont};
+  &::placeholder {
+    color: ${(props) => props.theme.colors.colorMainFont};
+  }
   &:focus {
-    border: 0.1rem solid var(--Brown-40, #542f1a);
+    border: 0.1rem solid ${(props) => props.theme.colors.colorBrown_40};
   }
 `;
 
